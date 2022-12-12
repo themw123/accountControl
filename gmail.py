67af879f7caa9f734c54d7c46d7419ef50ad3f72
themw123 <marvin.w6@web.de>
2022-12-12 23:45:53 +0100

    n

--- gmail.py
+++ gmail.py
@@ -14,9 +14,13 @@ class Gmail:
 
     def create_account(self):
         url = "https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp"
+        headers = {
+            'User-Agent': self.request.create_user_agent(),
+            'From': 'youremail@domain.example'
+        }
         print(self.person.first_name)
         print(self.person.last_name)
         print(self.person.user_name)
         print(self.person.password)
         body = ""
-        response = self.request.make_request(url, body)
+        response = self.request.make_request(url, headers, body)
