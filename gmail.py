65b20c8a61061900c218f01d4be50ebba21945e0
themw123 <marvin.w6@web.de>
2022-12-13 20:34:21 +0100

    n

--- gmail.py
+++ gmail.py
@@ -4,7 +4,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from seleniumwire import webdriver
 
 from person import Person
 from request import Request
