774a8faf297a63d0b2aa675fdf86a76d82c7be4a
themw123 <marvin.w6@web.de>
2022-12-13 08:52:06 +0100

    username random

--- person.py
+++ person.py
@@ -1,17 +1,26 @@
+import random
+import string
+
 import names
+from faker import Faker
 
 
 class Person:
 
+    fake: Faker
     first_name: str
     last_name: str
     user_name: str
     password = "***REMOVED***"
 
     def __init__(self):
+        self.fake = Faker()
         self.create_names()
 
     def create_names(self):
-        self.first_name = names.get_first_name()
-        self.last_name = names.get_last_name()
-        self.user_name = self.first_name + self.last_name
+        self.first_name = self.fake.first_name()
+        self.last_name = self.fake.last_name()
+        self.user_name = self.fake.user_name()
+        letter = random.choice(string.ascii_letters)
+        number = random.randint(0, 10)
+        self.user_name = self.user_name + letter + str(number)
