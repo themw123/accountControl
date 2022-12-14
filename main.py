f1dca083134f864a3cd2dcfa44264cdc29a7831f
themw123 <marvin.w6@web.de>
2022-12-15 00:08:00 +0100

    n

--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 from consolemenu import *
 from consolemenu.items import *
 
+from gmail import Gmail
+from mongodb import Mongodb
 from person import Person
 
+mongodb = Mongodb()
 person = Person()
+gmail = Gmail(person, mongodb)
+
+
 menu = ConsoleMenu("Welcome to AccountGen", "Version 1.0")
 function_item1 = FunctionItem("Generate Fake Person", person.gen_fake_person)
-function_item2 = FunctionItem("Show All", person.show_all_gmail_database)
+function_item2 = FunctionItem("Show All", gmail.show_all_database)
 function_item3 = FunctionItem(
-    "Show all latest Inbox", person.show_all_latest_inbox_gmail_database)
+    "Show all latest Inbox", gmail.show_all_latest_inbox_database)
 
 selection_menu_gmail = SelectionMenu("", "Gmail")
 selection_menu_gmail.append_item(function_item2)
