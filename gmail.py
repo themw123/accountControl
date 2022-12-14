892149a6cf73878cd03939190465d061c4831653
themw123 <marvin.w6@web.de>
2022-12-14 23:43:25 +0100

    n

--- gmail.py
+++ gmail.py
@@ -1,27 +1,52 @@
-import random
-import time
 
-import undetected_chromedriver as uc
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
+from __future__ import print_function
 
-from person import Person
-from request import Request
+import base64
+import os.path
+from email.mime.text import MIMEText
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Gmail:
     # Class attribute
-    person: Person
-    request: Request
 
     # Constructor
-    def __init__(self, request, person):
-        self.request = request
-        self.person = person
 
-    def set_sessions(self):
-        pass
+    def print_inbox(self):
+        SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+        creds = None
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+                token = input("enter token.json as string: ")
+                self.person.save_token_gmail_database(token)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
 
-    def check_sessions(self):
-        pass
+        try:
+            # Call the Gmail API
+            service = build('gmail', 'v1', credentials=creds)
+            response = service.users().messages().list(
+                userId="me", labelIds=["INBOX"], maxResults=1).execute()
+            message = response.get("messages")[0]
+            # Print the subject and body of the email
+            message_details = service.users().messages().get(
+                userId="me", id=message["id"]).execute()
+            # print(self.person["user_name"])
+            print("Subject:", message_details["snippet"])
+            print("\n")
+        except HttpError as error:
+            # TODO(developer) - Handle errors from gmail API.
+            print(f'An error occurred: {error}')
--- main.py
+++ main.py
@@ -4,17 +4,20 @@ from consolemenu.items import *
 
 from person import Person
 
-
 person = Person()
 menu = ConsoleMenu("Welcome to AccountGen", "Version 1.0")
 function_item1 = FunctionItem("Generate Fake Person", person.gen_fake_person)
-function_item2 = FunctionItem("Show_all", person.show_all_database)
+function_item2 = FunctionItem("Show All", person.show_all_gmail_database)
+function_item3 = FunctionItem(
+    "Show all latest Inbox", person.show_all_latest_inbox_gmail_database)
 
-selection_menu = SelectionMenu("")
-selection_menu.append_item(function_item2)
-submenu_item = SubmenuItem("Accounts", selection_menu, menu)
+selection_menu_gmail = SelectionMenu("", "Gmail")
+selection_menu_gmail.append_item(function_item2)
+selection_menu_gmail.append_item(function_item3)
+
+submenu_item_gmail = SubmenuItem("Gmail", selection_menu_gmail, menu)
 
 
 menu.append_item(function_item1)
-menu.append_item(submenu_item)
+menu.append_item(submenu_item_gmail)
 menu.show()
