03a97418e6701413ab132098cbf9719f981d298e
themw123 <marvin.w6@web.de>
2022-12-14 10:53:22 +0100

    n

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 
+import time
+
+from consolemenu import *
+from consolemenu.items import *
+
 from mongodb import Mongodb
 from person import Person
 
@@ -15,44 +20,30 @@ def save_person_in_database():
         mongodb.set_collection("gmail")
         mongodb.insert(data)
         print("saved.")
-        print("\n")
+        time.sleep(2)
     elif number_database == "n":
-        print("not saved to database")
-        print("\n")
+        print("not saved.")
     return number_database
 
 
-print("\nWelcome to AccountGen 1.0\n")
+def gen_fake_person():
+    person.create_names()
+    person.print_person()
+    print("\n")
+    number_database = ""
+    while not number_database or (number_database not in ["y", "n"]):
+        number_database = save_person_in_database()
+
+
 mongodb = Mongodb()
 person = Person()
 
-options = "[1]: generate fake person \n[2]: comming soon \n[3]: exit"
-while True:
-    print("What do you want to do?")
-    user_input = input(options + "\n" + "choose: ")
+menu = ConsoleMenu("Welcome to AccountGen", "Version 1.0")
+function_item = FunctionItem(
+    "Generate Fake Person", gen_fake_person)
+selection_menu = SelectionMenu(["Show all", "item2", "item3"])
+submenu_item = SubmenuItem("Accounts", selection_menu, menu)
 
-    if not user_input.isdigit():
-        print("!!!Please enter a number!!!")
-        print("\n")
-        continue
-
-    number = int(user_input)
-
-    if number not in range(1, 4):
-        print("!!!no option found!!!")
-        print("\n")
-        continue
-
-    if number == 3:
-        break
-
-    if number == 1:
-        person.create_names()
-        person.print_person()
-        print("\n")
-        number_database = ""
-        while not number_database or (number_database not in ["y", "n"]):
-            number_database = save_person_in_database()
-    if number == 2:
-        print("comming soon")
-        print("\n")
+menu.append_item(function_item)
+menu.append_item(submenu_item)
+menu.show()
