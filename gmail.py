dc03907096ab5ef72e224030c5a46c8ee55d435c
themw123 <marvin.w6@web.de>
2022-12-15 18:29:46 +0100

    n

--- gmail.py
+++ gmail.py
@@ -5,7 +5,6 @@ import base64
 import json
 from email.message import EmailMessage
 
-from bs4 import BeautifulSoup
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
