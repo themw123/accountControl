40fd00e913a158da3f8504d9b061f85b3d172b56
themw123 <marvin.w6@web.de>
2022-12-18 11:06:35 +0100

    n

--- Main.py
+++ Main.py
@@ -9,7 +9,6 @@ gmail = Gmail()
 
 menu = ConsoleMenu("Welcome to AccountControl", "Version 1.0")
 function_item1 = FunctionItem(
-
     "Generate Fake Person", gmail.gen_fake_person)
 function_item2 = FunctionItem("Show accounts", gmail.show_all_database)
 function_item3 = FunctionItem(
